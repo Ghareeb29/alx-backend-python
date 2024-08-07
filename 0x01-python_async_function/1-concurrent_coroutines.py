d113eb26cd9fbc31481099c17eee3ffc9c57904d
Ghareeb29 <sonic_unleashed@live.com>
2024-08-07 19:36:19 +0300

    style fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,12 +2,14 @@
 """ Concurrent Coroutines """
 import asyncio
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """returns the list of all the delays (float values)."""
     delays = []
+
     async def add_delay():
         delay = await wait_random(max_delay)
         for i, d in enumerate(delays):
@@ -15,6 +17,6 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
                 delays.insert(i, delay)
                 return
         delays.append(delay)
-    
+
     await asyncio.gather(*(add_delay() for _ in range(n)))
     return delays
