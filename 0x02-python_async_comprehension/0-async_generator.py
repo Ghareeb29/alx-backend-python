1d0aa334f996164ed94186b3ea353cd62e41775e
Ghareeb29 <sonic_unleashed@live.com>
2024-08-08 23:04:49 +0300

    Function type annotation fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float]:
     """async generator"""
     for i in range(10):
         await asyncio.sleep(1)
