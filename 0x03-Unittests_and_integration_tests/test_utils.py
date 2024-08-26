d8f1e69aa79af37b5d8bdcae5c7728a87b462060
Ghareeb29 <sonic_unleashed@live.com>
2024-08-26 21:42:05 +0300

    Task 0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Tests for utils where we:
+- Create a TestAccessNestedMap class
+that inherits from unittest.TestCase
+
+- Implement the TestAccessNestedMap.test_access_nested_map method
+to test that the method returns what it is supposed to.
+
+- Decorate the method with @parameterized.expand
+to test the function with provided inputs
+"""
+from unittest import TestCase, mock
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+
+
+@parameterized.expand(
+    [
+        ({"a": 1}, ["a"], 1),
+        ({"a": {"b": 2}}, ["a"], {"b": 2}),
+        ({"a": {"b": 2}}, ["a", "b"], 2),
+    ]
+)
+class TestAccessNestedMap(TestCase):
+    """Unit testing class for utils.access_nested_map"""
+
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test that access_nested_map returns the expected value"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    def test_access_nested_map_exception(self):
+        """Test that access_nested_map raises an exception"""
+        with self.assertRaises(KeyError):
+            access_nested_map({}, ["a", "b"])
+
+if __name__ == "__main__":
+    unittest.main()
