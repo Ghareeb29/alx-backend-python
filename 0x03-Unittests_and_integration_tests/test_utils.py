a5f37b4bdefc955e7691434268a635c1df4c3324
Ghareeb29 <sonic_unleashed@live.com>
2024-08-31 02:00:26 +0300

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ Module for testing the utils module.
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Tuple, Union
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -35,6 +35,32 @@ class TestAccessNestedMap(unittest.TestCase):
             None
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), "a"),
+            ({"a": 1}, ("a", "b"), "b"),
+        ]
+    )
+    def test_access_nested_map_exception(
+        self, nested_map: Dict, path: Tuple[str], expected_exception_msg: str
+    ) -> None:
+        """
+        Test that access_nested_map
+        raises a KeyError with the expected message.
+
+        Args:
+            nested_map (Dict): The nested dictionary to access.
+            path (Tuple[str]): The path of keys to access the nested value.
+            expected_exception_msg (str): The expected exception message.
+
+        Returns:
+            None
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), f"'{expected_exception_msg}'")
 
 
 if __name__ == "__main__":
